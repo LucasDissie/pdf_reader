5c3e080b6103a3c415d54535deefb30a1ed09c80
LucasDissie <lucasdisseldorp@gmail.com>
2023-11-03 11:16:27 +0100

    Fluffybassoon functional

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 const pdfextract = require('pdf.js-extract');
 const grootboeken = require('./ProductenGrootboeken.json');
 const express = require('express');
+const fs = require('fs');
 
 const port = 5000;
 const app = express();
 
 const pdfExtract = new pdfextract.PDFExtract();
-var products;
+var invoices = [];
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
   res.sendFile('./public/index.html', {root: __dirname}); 
-  res.send(products);     
 });
 
-app.get('/products', (req, res) => {        //get requests to the root ("/") will route here
-  res.send(products);     
+app.get('/invoices', (req, res) => {        //get requests to the root ("/") will route here
+  res.send(invoices);     
 });
 
 app.listen(port, () => {            
   console.log(`Now listening on port ${port}`); 
 });
 
-function extractData() {
+function extractData(path) {
   return new Promise((resolve, reject) => {
     const options = {}; // Define your options
     const goodContent = [];
 
-    pdfExtract.extract('sample.pdf', options, (err, data) => {
+    pdfExtract.extract(path, options, (err, data) => {
       if (err) {
+        console.log(err)
         reject(err);
       } else {
         let startParsing = false;
@@ -44,6 +45,11 @@ function extractData() {
             }
             if (startParsing && !(data.pages[i].content[j].str === '' || data.pages[i].content[j].str === ' ' || data.pages[i].content[j].str === undefined)) {
               goodContent.push(data.pages[i].content[j].str);
+              if(data.pages[i].content[j].str.includes("RETOUR")){
+                goodContent.push("");
+                goodContent.push("");
+                goodContent.push("");
+              }
             }
             if (data.pages[i].content[j].str === "Excl. BTW") {
               startParsing = true;
@@ -57,9 +63,9 @@ function extractData() {
 }
 
 
-async function getProducts() {
+async function getProducts(path) {
   try {
-    const goodContent = await extractData();
+    const goodContent = await extractData(path);
     return goodContent;
   } catch (err) {
     console.error(err);
@@ -83,11 +89,27 @@ function parseProducts(data) {
   return products;
 }
 
-getProducts().then(data => {
-  products = parseProducts(data);
-  console.log(products);
-}) 
+// getProducts().then(data => {
+//   invoices.push(parseProducts(data));
+// }) 
 
 
-
+function parseAllInvoices(){
+  const directoryPath = './pdfs'; // Replace with the path to your directory
+  let fileArray = [];
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+  
+    fileArray = files.filter(file => fs.statSync(`${directoryPath}/${file}`).isFile());
+    for (let index = 0; index < fileArray.length; index++) {
+      getProducts(`./pdfs/${fileArray[index]}`).then(data => {
+        invoices.push(parseProducts(data));
+      })
+    }
+  });
+}
+parseAllInvoices();
 
--- public/client.js
+++ public/client.js
@@ -1,42 +1,92 @@
-console.log("test");
-
-let url = "http://localhost:5000/products";
-let products = [];
+let url = "http://localhost:5000/invoices";
+let invoices = [];
+let currentInvoice = 0;
+window.onload = function() { setupElements(); }
 fetch(url).then(function(response) {
   return response.json();
 }).then(function(data) {
-  products = data;
-  populateTable(products);
-  calculatePrices(products);
+  invoices = data;
+  populateTable(invoices[currentInvoice]);
+  calculatePrices(invoices[currentInvoice]);
 }).catch(function(err) {
   console.log('Fetch Error :-S', err);
 });
+
+function setupElements(){
+  let next = document.getElementById("next");
+  next.addEventListener("click", nextInvoice);
+  let previous = document.getElementById("previous");
+  previous.addEventListener("click", previousInvoice);
+}
+function nextInvoice(){
+  if(currentInvoice < invoices.length - 1){
+    currentInvoice++;
+    clearTables();
+    populateTable(invoices[currentInvoice]);
+    calculatePrices(invoices[currentInvoice]);
+  }
+  else{
+    currentInvoice = 0;
+    clearTables();
+    populateTable(invoices[currentInvoice]);
+    calculatePrices(invoices[currentInvoice]);
+  }
+}
+
+function previousInvoice(){
+  if(currentInvoice > 0){
+    currentInvoice--;
+    clearTables();
+    populateTable(invoices[currentInvoice]);
+    calculatePrices(invoices[currentInvoice]);
+  }
+  else{
+    currentInvoice = invoices.length - 1;
+    clearTables();
+    populateTable(invoices[currentInvoice]);
+    calculatePrices(invoices[currentInvoice]);
+  }
+}
+
+function clearTables(){
+  let table = document.getElementById("productTable");
+  table.innerHTML = "";
+  let totalTable = document.getElementById("totalTable");
+  totalTable.innerHTML = "";
+}
+
 function populateTable(products){
   let table = document.getElementById("productTable");
+  let tr = document.createElement("tr");
+  tr.appendChild(document.createElement("th")).innerHTML = "Product";
+  tr.appendChild(document.createElement("th")).innerHTML = "Aantal";
+  tr.appendChild(document.createElement("th")).innerHTML = "Prijs";
+  tr.appendChild(document.createElement("th")).innerHTML = "Eenheid";
+  tr.appendChild(document.createElement("th")).innerHTML = "BTW";
+  tr.appendChild(document.createElement("th")).innerHTML = "Totaal";
+  tr.appendChild(document.createElement("th")).innerHTML = "Grootboek";
+  tr.appendChild(document.createElement("th")).innerHTML = "Statiegeld";
+  table.appendChild(tr);
   for (let index = 0; index < products.length; index++) {
     let row = document.createElement("tr");
+    row.setAttribute("id", index);
     row.appendChild(document.createElement("td")).innerHTML = products[index].name;
     row.appendChild(document.createElement("td")).innerHTML = products[index].amount;
     row.appendChild(document.createElement("td")).innerHTML = products[index].price;
     row.appendChild(document.createElement("td")).innerHTML = products[index].unit;
     row.appendChild(document.createElement("td")).innerHTML = products[index].tax;
     row.appendChild(document.createElement("td")).innerHTML = products[index].total;
+    let tgrootboek = document.createElement("td")
     if(products[index].grootboek == undefined){
-      let td = document.createElement("td")
-      td.style.backgroundColor = "red";
-      row.appendChild(td).innerHTML = products[index].grootboek;
+      tgrootboek.style.backgroundColor = "red";
     }
-    else{
-      row.appendChild(document.createElement("td")).innerHTML = products[index].grootboek;
-    }
+    tgrootboek.appendChild(grootboekDropdown(products[index].grootboek))
+    row.appendChild(tgrootboek)
+    let tstatiegeld = document.createElement("td")
     if(products[index].statiegeld == undefined){
-      let td = document.createElement("td")
-      td.style.backgroundColor = "red";
-      row.appendChild(td).innerHTML = products[index].grootboek;
+      tstatiegeld.style.backgroundColor = "red";
     }
-    else{
-      row.appendChild(document.createElement("td")).innerHTML = products[index].statiegeld;
-    }
+    row.appendChild(tstatiegeld).innerHTML = products[index].grootboek;
     table.appendChild(row);
   }
 }
@@ -58,31 +108,81 @@ function calculatePrices(products){
       "21 %": 0.0,
       "0 %": 0.0
     },
-    "Emballage":{
-      "0 %": 0.0,
-      "retour": 0.0
-    },
     "Algemene kosten":{
       "9 %": 0.0,
       "21 %": 0.0,
       "0 %": 0.0
+    },
+    "Emballage":{
+      "0 %": 0.0,
+      "Retour": 0.0
     }
   }
   for (let index = 0; index < products.length; index++) {
     if(products[index].grootboek == undefined || products[index].statiegeld == undefined){
       continue
     }
-    prices[products[index].grootboek][products[index].tax] += parseFloat(products[index].total.replace(',','.'));
+    if(products[index].total.endsWith("-")){
+      if(products[index].grootboek == "Emballage"){
+        prices[products[index].grootboek]["Retour"] -= parseFloat(products[index].total.replace(',','.'));
+      }
+      else{
+        prices[products[index].grootboek][products[index].tax] -= parseFloat(products[index].total.replace(',','.'));
+      }
+    }
+    else{
+      prices[products[index].grootboek][products[index].tax] += parseFloat(products[index].total.replace(',','.'));
+    }
   }
   let table = document.getElementById("totalTable");
+  let row = document.createElement("tr");
+  row.appendChild(document.createElement("th")).innerHTML = "Grootboek";
+  row.appendChild(document.createElement("th")).innerHTML = "9 %";
+  row.appendChild(document.createElement("th")).innerHTML = "21 %";
+  row.appendChild(document.createElement("th")).innerHTML = "0 %";
+  row.appendChild(document.createElement("th")).innerHTML = "Retour";
+  table.appendChild(row);
   let grootboeken= Object.keys(prices);
   for (let index = 0; index < grootboeken.length; index++) {
     let row = document.createElement("tr");
-    row.appendChild(document.createElement("td")).innerHTML = grootboeken[index];
-    row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["9 %"].toFixed(2);
-    row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["21 %"].toFixed(2);
-    row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["0 %"].toFixed(2);
+    if(grootboeken[index] != "Emballage"){
+      row.appendChild(document.createElement("td")).innerHTML = grootboeken[index];
+      row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["9 %"].toFixed(2);
+      row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["21 %"].toFixed(2);
+      row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["0 %"].toFixed(2);
+      row.appendChild(document.createElement("td")).innerHTML = ""
+    }
+    else{
+      row.appendChild(document.createElement("td")).innerHTML = grootboeken[index];
+      row.appendChild(document.createElement("td")).innerHTML = "";
+      row.appendChild(document.createElement("td")).innerHTML = "";
+      row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["0 %"].toFixed(2);
+      row.appendChild(document.createElement("td")).innerHTML = prices[grootboeken[index]]["Retour"].toFixed(2);
+    }
     table.appendChild(row);
-    
   }
+}
+
+function grootboekDropdown(selectedValue){
+  let dropDown = document.createElement("select");
+  let options = ["Kamer", "Mongoose producten", "Borrels", "Algemene kosten", "Emballage"];
+  for (let index = 0; index < options.length; index++) {
+    let option = document.createElement("option");
+    option.setAttribute("value", options[index]);
+    option.innerHTML = options[index];
+    if(options[index] == selectedValue){
+      option.selected = true;
+    }
+    dropDown.appendChild(option);
+  }
+  dropDown.onchange = changeGrootboek;
+    
+  return dropDown
+}
+
+function changeGrootboek(e){
+  invoices[currentInvoice][e.target.parentNode.parentNode.id].grootboek = e.target.value;
+  clearTables();
+  populateTable(invoices[currentInvoice]);
+  calculatePrices(invoices[currentInvoice]);
 }
